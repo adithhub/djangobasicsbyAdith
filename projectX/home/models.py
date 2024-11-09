251ece5c46a1a2a1eb65752940d885ec170849f4
Adith <adithyancb3@gmail.com>
2024-11-09 10:20:29 +0530

    python shell

--- projectX/home/models.py
+++ projectX/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=100)
+    address = models.TextField(null=True, blank=True)
+    image = models.ImageField()
+    file = models.FileField()
+  
